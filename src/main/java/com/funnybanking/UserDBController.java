f136a6900e3c087b8091a6ebefb8a4df34950fe4
Scarfacehbc <p.jipp87@gmail.com>
2022-06-13 08:48:13 +0200

    ......

--- src/main/java/com/funnybanking/UserDBController.java
+++ src/main/java/com/funnybanking/UserDBController.java
@@ -1,5 +1,22 @@
 package com.funnybanking;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.*;
 
 public class UserDBController {
-    
+    static Connection con;
+    static Statement stm;
+
+    public static void connectToTB(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.out.println();
+        }
+    }
 }
+
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.funnybanking {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens com.funnybanking to javafx.fxml;
     exports com.funnybanking;
