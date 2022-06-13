b61abe2bab5498bf821ff88e84815340f82852ba
Scarfacehbc <p.jipp87@gmail.com>
2022-06-13 10:19:58 +0200

    ......

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.funnybanking {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires mysql.connector.java;
+    
 
     opens com.funnybanking to javafx.fxml;
     exports com.funnybanking;
